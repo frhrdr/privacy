80070471ed4fb308900de8130fd57120164f7240
frhrdr <frederikharder@gmail.com>
2019-09-03 11:29:25 +0200

    made big cnn easier to train

---
+++ tutorials/collect_accuracies.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def main():
+  base_str = 'models_fmnist/big_private_lr{}_clip{}_mb32 --microbatches 32'
+  pairs_1 = [(k, '1.5') for k in [0.01, 0.02, 0.04, 0.005, 0.002, 0.001]]
+  pairs_2 = [(k, '3.0') for k in [0.01, 0.02, 0.04]]
+  pairs = pairs_1 + pairs_2
+  for lr, clip in pairs:
+    load_str = base_str.format(lr, clip)
+    mat = np.load(load_str)
+    print(load_str)
+    print(mat)
+
+  for lr, clip in pairs:
+    load_str = base_str.format(lr, clip)
+    mat = np.load(load_str)
+    print(load_str)
+    print(mat[-1])
+
+
+if __name__ == '__main__':
+  main()
